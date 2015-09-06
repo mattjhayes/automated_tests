6898675ded005e6e0bafa5df132928a24bc47ef7
Bob <bob@tc1.example.com>
2015-09-06 13:27:59 +1200

    Adding files used for baseline of SDN test environment

---
+++ baseline-combined.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Run baseline tests
+"""
+import datetime
+import time
+import os
+import sys
+
+version = "0.1.0"
+#*** How many times to run the set of tests:
+repeats = 5
+#*** Types of tests to run:
+tests = ["baseline-nmeta", "baseline-simpleswitch", "baseline-nosdn"]
+results_dir="/home/bob/results/baseline-combined/"
+#*** Ansible Playbook to use:
+playbook = "/home/bob/automated_tests/baseline-template.yml"
+
+#*** Timestamp
+timenow = datetime.datetime.now()
+timestamp = timenow.strftime("%Y%m%d%H%M%S")
+print "timestamp is", timestamp
+
+#*** Create main folder for results:
+os.chdir(results_dir)
+os.mkdir(timestamp)
+test_basedir = os.path.join(results_dir, timestamp)
+print "test_basedir is", test_basedir
+
+#*** Create sub folders
+os.chdir(test_basedir)
+for test in tests:
+    os.mkdir(test)
+
+#*** Run tests
+for i in range(repeats):
+    for test in tests:
+        print "running test", test
+        test_dir=os.path.join(test_basedir, test)
+        if test == "baseline-nmeta":
+            start_nmeta="true"
+            start_simple_switch="false"
+        elif test == "baseline-simpleswitch":
+            start_nmeta="false"
+            start_simple_switch="true"
+        elif test == "baseline-nosdn":
+            start_nmeta="false"
+            start_simple_switch="false"
+        else:
+            print "ERROR: unknown test type", test
+            sys.exit()
+        playbook_cmd = "ansible-playbook " + playbook + " --extra-vars "
+        playbook_cmd += "\"start_nmeta=" + start_nmeta
+        playbook_cmd += " start_simple_switch=" + start_simple_switch
+        playbook_cmd += " results_dir=" + test_dir + "/\""
+        print "playbook_cmd is", playbook_cmd
+        
+        print "running Ansible playbook..."
+        os.system(playbook_cmd)
+        print "Sleeping... zzzz"
+        time.sleep(60)
+        
