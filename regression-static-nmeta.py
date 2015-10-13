d7ada9ef7814c67d5b615a32a8e77d0681a188ad
mattjhayes <matthew_john_hayes@hotmail.com>
2015-10-13 22:33:34 +1300

    update from mattjhayes

---
+++ regression-static-nmeta.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Run nmeta static TC regression tests
+"""
+import datetime
+import time
+import os
+from os.path import expanduser
+import sys
+
+version = "0.1.0"
+
+#*** How many times to run the set of tests:
+repeats = 2
+
+#*** Types of tests to run:
+tests = ["unconstrained-bw", "constrained-bw"]
+
+#*** Directory base path to write results to:
+home_dir = expanduser("~")
+results_dir = os.path.join(home_dir, "results/regression/nmeta-static/")
+
+#*** Parameters for regression test:
+duration="10"
+
+#*** Ansible Playbook to use:
+playbook = os.path.join(home_dir, \
+                    "automated_tests/regression-static-template.yml")
+
+#*** Timestamp for results root directory:
+timenow = datetime.datetime.now()
+timestamp = timenow.strftime("%Y%m%d%H%M%S")
+print "timestamp is", timestamp
+
+#*** Create root directory for results:
+os.chdir(results_dir)
+os.mkdir(timestamp)
+test_basedir = os.path.join(results_dir, timestamp)
+print "test_basedir is", test_basedir
+
+#*** Create sub folders
+os.chdir(test_basedir)
+for test in tests:
+    os.mkdir(test)
+
+#*** Run tests
+for i in range(repeats):
+    for test in tests:
+        print "running test", test
+        test_dir=os.path.join(test_basedir, test)
+        if test == "unconstrained-bw":
+            start_nmeta="true"
+            tcp_port="5555"
+        elif test == "constrained-bw":
+            start_nmeta="true"
+            tcp_port="1234"
+        else:
+            print "ERROR: unknown test type", test
+            sys.exit()
+        playbook_cmd = "ansible-playbook " + playbook + " --extra-vars "
+        playbook_cmd += "\"start_nmeta=" + start_nmeta
+        playbook_cmd += " duration=" + duration
+        playbook_cmd += " results_dir=" + test_dir + "/"
+        playbook_cmd += " tcp_port=" + tcp_port
+        playbook_cmd += "\""
+        print "playbook_cmd is", playbook_cmd
+        
+        print "running Ansible playbook..."
+        os.system(playbook_cmd)
+        print "Sleeping... zzzz"
+        time.sleep(30)
+        
