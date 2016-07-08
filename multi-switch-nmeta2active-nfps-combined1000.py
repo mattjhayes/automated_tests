9f8a77f00f5fde7324703814da1f09b7bf3f47d8
mattjhayes <matthew_john_hayes@hotmail.com>
2016-07-09 08:49:41 +1200

    update from mattjhayes

--- multi-switch-nmeta2active-nfps-combined1000.py
+++ multi-switch-nmeta2active-nfps-combined1000.py
@@ -57,7 +57,7 @@ def main():
     """
 
     #*** Types of tests to run (number of switches in path):
-    tests = ["single", "dual"]
+    tests = ["1", "2", "3"]
 
     #*** Parameters for filt new flow rate load test:
     target_ip = "10.1.0.7"
@@ -78,6 +78,8 @@ def main():
             "automated_tests/multi-switch-setup-single-switch.yml")
     playbook_dual_switch = os.path.join(HOME_DIR, \
             "automated_tests/multi-switch-setup-dual-switch.yml")
+    playbook_triple_switch = os.path.join(HOME_DIR, \
+            "automated_tests/multi-switch-setup-triple-switch.yml")
 
     #*** Create sub folders
     os.chdir(TEST_BASEDIR)
@@ -100,13 +102,19 @@ def main():
                                                         "of", REPEATS)
             test_dir = os.path.join(TEST_BASEDIR, test)
             #*** Set switches up appropriate to test type:
-            if test == "single":
+            if test == "1":
                 playbook_cmd = "ansible-playbook " + playbook_single_switch
                 result = os.system(playbook_cmd)
                 result = test + "," + str(i+1) + "," + str(result)
                 write_result(FILENAME_SWITCH_SETUP_RESULTS, result)
-            elif test == "dual":
+            elif test == "2":
                 playbook_cmd = "ansible-playbook " + playbook_dual_switch
+                result = os.system(playbook_cmd)
+                result = test + "," + str(i+1) + "," + str(result)
+                write_result(FILENAME_SWITCH_SETUP_RESULTS, result)
+                start_dpn = 1
+            elif test == "3":
+                playbook_cmd = "ansible-playbook " + playbook_triple_switch
                 result = os.system(playbook_cmd)
                 result = test + "," + str(i+1) + "," + str(result)
                 write_result(FILENAME_SWITCH_SETUP_RESULTS, result)
